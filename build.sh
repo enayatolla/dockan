7f2a97edc3f9b3277d741a31a9526144b7c65f70
enayatullah rezaie <enayatulla@enayatullahs-MacBook-Air.local>
2025-05-30 21:56:33 +0330

    new change3

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations
 python manage.py migrate
 
 # python -m gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker
