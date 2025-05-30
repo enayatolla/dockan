ee02f41d67d53bf79486809ab1881e4e34170b00
enayatullah rezaie <enayatulla@enayatullahs-MacBook-Air.local>
2025-05-30 21:33:20 +0330

    new change

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,18 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key')
 
 DATABASES = {
    'default': dj_database_url.config(
-      default= os.getenv('DJANGO_DATABASE_URL'),
-      conn_max_age=600
+      default= os.getenv('DJNAGO_DATABASE_URL'),
    )
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-   # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
